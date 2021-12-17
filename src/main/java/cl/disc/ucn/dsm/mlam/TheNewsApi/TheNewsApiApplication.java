bed92719285fea6775d871f8270f2398bd07cbed
mlam <marcelo.lam@alumnos.ucn.cl>
2021-12-17 19:43:23 -0300

    chore(project): initial commit

---
+++ src/main/java/cl/disc/ucn/dsm/mlam/TheNewsApi/TheNewsApiApplication.java
@@ -0,0 +1,13 @@
+package cl.disc.ucn.dsm.mlam.TheNewsApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheNewsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheNewsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/disc/ucn/dsm/mlam/TheNewsApi/TheNewsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.disc.ucn.dsm.mlam.TheNewsApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheNewsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
