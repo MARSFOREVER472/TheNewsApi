aae6b717525ca10d6de647722777ce948be06d80
mlam <marcelo.lam@alumnos.ucn.cl>
2021-12-20 22:30:47 -0300

    feat(application): add initializing bean

--- src/main/java/cl/disc/ucn/dsm/mlam/TheNewsApi/TheNewsApiApplication.java
+++ src/main/java/cl/disc/ucn/dsm/mlam/TheNewsApi/TheNewsApiApplication.java
@@ -1,13 +1,28 @@
 package cl.disc.ucn.dsm.mlam.TheNewsApi;
 
+import cl.disc.ucn.dsm.mlam.TheNewsApi.service.NewsApiService;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TheNewsApiApplication {
+
+	@Autowired
+	private NewsRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TheNewsApiApplication.class, args);
 	}
 
+	@Bean
+	protected InitializingBean initializeDatabase(){
+		return ()->{
+			NewsApiService service = new NewsApiService();
+			this.repository.saveAll(service.getNews(100));
+		};
+	}
 }
