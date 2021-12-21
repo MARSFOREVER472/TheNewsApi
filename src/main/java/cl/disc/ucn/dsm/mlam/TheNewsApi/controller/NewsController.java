699fb66f293e5af9cb8b1bd2862395173c7ded63
mlam <marcelo.lam@alumnos.ucn.cl>
2021-12-20 22:28:34 -0300

    feat(controller): implement methods with repository

--- src/main/java/cl/disc/ucn/dsm/mlam/TheNewsApi/controller/NewsController.java
+++ src/main/java/cl/disc/ucn/dsm/mlam/TheNewsApi/controller/NewsController.java
@@ -1,7 +1,9 @@
 package cl.disc.ucn.dsm.mlam.TheNewsApi.controller;
 
+import cl.disc.ucn.dsm.mlam.TheNewsApi.NewsRepository;
 import cl.disc.ucn.dsm.mlam.TheNewsApi.model.Article;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -12,14 +14,37 @@ import java.util.List;
  */
 @RestController
 public class NewsController {
+    private NewsRepository repository;
+
+    /**
+     * Constructor.
+     *
+     * @param repository
+     */
+    public NewsController(NewsRepository repository) {
+        this.repository = repository;
+    }
 
     /**
      * Return a list of articles.
+     *
      * @return
      */
     @GetMapping("/v1/news")
-    public List<Article>getAll(){
+    public List<Article> getAll() {
 
-        return new ArrayList<Article>();
+        return this.repository.findAll();
+    }
+
+    /**
+     * Return the article by id.
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/v1/news/{id}")
+    public Article getArticle(@PathVariable Long id) {
+        return this.repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Not found 404"));
     }
 }
